8165c3fbdf6672460ed1db0aa22d6702e3fa0f89
swinture <837928354@qq.com>
2018-11-06 19:06:28 +0800

    初始化版本

---
+++ src/main/java/com/swinture/thirdparty/login/ThirdPartyLoginApplication.java
@@ -0,0 +1,12 @@
+package com.swinture.thirdparty.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPartyLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdPartyLoginApplication.class, args);
+    }
+}
